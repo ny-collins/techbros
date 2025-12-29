58ed318921aebf819dbcb6e228e7778ec01e098b
Collins Mwangi <mwangicollins391@gmail.com>
2025-12-29 05:57:33 +0300

    Added 404 page

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        notFound: resolve(__dirname, '404.html'),
       },
     },
   },
